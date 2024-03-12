dd97d3c6d7eaa0f523e85829bffc5d0485fc6643
Justin Leto <leto.justin@gmail.com>
2024-03-12 14:56:42 -0400

    Update crew.sql
    updated placeholder label from [PREFIX] to [REGION]

--- psql/sql/crew.sql
+++ psql/sql/crew.sql
@@ -4,7 +4,7 @@ SET LOCAL synchronous_commit TO OFF;
 
 SELECT aws_s3.table_import_from_s3(
  'staging.crew', '', '(FORMAT csv, HEADER true, DELIMITER E''\t'')',
- '[S3_BUCKET_NAME]', '[S3_PRFEIX]/title.crew.tsv.gz', '[PREFIX]'
+ '[S3_BUCKET_NAME]', '[S3_PRFEIX]/title.crew.tsv.gz', '[REGION]'
 );
 
 commit;
